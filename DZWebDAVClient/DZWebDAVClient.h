bed46ffa6c4397c7a09ec34bbfaa97f637da003f
Dan Browne <daniel.browne@springer.com>
2016-01-28 15:02:52 +0000

    Reformats imports for the fact that AFNetworking is now a framework.

--- DZWebDAVClient/DZWebDAVClient.h
+++ DZWebDAVClient/DZWebDAVClient.h
@@ -8,7 +8,7 @@
 //  Licensed under MIT. See LICENSE.
 //
 
-#import "AFHTTPClient.h"
+@import AFNetworking.AFHTTPClient;
 
 /** The key for a uniform (MIME) type identifier returned from the property request methods. */
 extern NSString *DZWebDAVContentTypeKey;
